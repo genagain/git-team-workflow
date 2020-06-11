801f7e6ed1f6ad263afcd6099d32709a6ca9efbb
Jesse <jesse.mwarren@yahoo.com>
2020-06-11 09:20:56 -0700

    Modified route for tasks management to meet conventions

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get("/api/secure/back-end/routes/management/tasks/", (request, response) => {
+app.get("/api/tasks/", (request, response) => {
   response.send("Tasks management!");
 })
 
