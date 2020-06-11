c2de06d4f341e26c6cd1687f5fb2d0db1a1a72e8
Jesse <jesse.mwarren@yahoo.com>
2020-06-11 09:14:41 -0700

    Added task management routes

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get("/api/secure/back-end/routes/management/tasks/", (request, response) => {
+  response.send("Tasks management!");
+})
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
